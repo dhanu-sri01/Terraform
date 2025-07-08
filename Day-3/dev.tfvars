7d90981ff255d15296de1c4b21275c87baecf86c
dhanu-sri01 <dhanusribattula@gmail.com>
2025-07-08 16:10:49 +0530

    day3

---
+++ Day-3/dev.tfvars
@@ -0,0 +1,1 @@
+instance = "t3.large"
---
+++ Day-3/variables.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+    default = "ami-00c8ac9147e19828e"
+    type = string
+}
+
+variable "instance" {
+    default = "t3.nano"
+    type = string
+}
