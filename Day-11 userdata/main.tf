9c241f2ec9836a06fec26e5adbbf6040fdc94af2
Dhanu <dhanusribattula@gmail.com>
2025-07-19 14:18:30 +0530

    day11

---
+++ Day-11 userdata/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "name" {
+    ami = "ami-0150ccaf51ab55a51"
+    instance_type = "t2.micro"
+    user_data = file("state.sh")
+    tags = {
+      Name ="dev"
+    }
+
+  
+}
+
