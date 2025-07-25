0234e86b1cafecec3a6436ff68cb16113f24ef78
Dhanu <dhanusribattula@gmail.com>
2025-07-25 17:45:05 +0530

    jenkins

--- Day-11 userdata/main.tf
+++ Day-11 userdata/main.tf
@@ -5,10 +5,12 @@ resource "aws_instance" "name" {
     ami = "ami-0150ccaf51ab55a51"
     instance_type = "t2.micro"
     user_data = file("state.sh")
+    vpc_security_group_ids = ["sg-0f5373e7a72195a1f"]
     tags = {
       Name ="dev"
     }
 
   
 }
+
 
